816ef96b1d2953b680c4cae97ff10846f18ca4df
Pavelprof <vasilevpl@yandex.ru>
2023-03-07 19:02:00 +0300

    second commit

--- portpy/portfolio/models.py
+++ portpy/portfolio/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-class Asset(models.Model):
+class Assets(models.Model):
     ticker = models.CharField(max_length=255)
     description = models.TextField()
     isin = models.CharField(max_length=255)
-    assetName = models.CharField(max_length=255)
-    icon =
+    issuer = models.CharField(max_length=255)
+    icon = models.ImageField(upload_to="icons/")
     currency = models.CharField(max_length=255)
     settlementCurrency = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
     assetType = models.CharField(max_length=255)
-    isTradeble = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField
+    isTradable = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- portpy/portpy/settings.py
+++ portpy/portpy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@*l_fs(69$z!q+(4-qn8#3bv8=c(a%tj-h59@308@kie3uew6w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
