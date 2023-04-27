3fb244ec4aaaea3fb964e0d086436cea89067129
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-04-27 09:50:19 +0300

    Models refactoring. Account model is created

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,14 +4,14 @@ from simple_history.models import HistoricalRecords
 
 
 class Asset(models.Model):
-    ticker = models.CharField(max_length=255)
-    isin = models.CharField(max_length=255, unique=True)
-    asset_name = models.CharField(max_length=255)
-    issuer = models.CharField(max_length=255)
+    ticker = models.CharField(max_length=20)
+    isin = models.CharField(max_length=50, unique=True)
+    asset_name = models.CharField(max_length=50)
+    issuer = models.CharField(max_length=100)
     icon = models.ImageField(upload_to="icons/", null=True)
-    settlement_currency = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    asset_type = models.CharField(max_length=255)
+    currency_influence = models.ForeignKey('Asset', related_name='+', on_delete=models.PROTECT)
+    country = models.CharField(max_length=50)
+    asset_type = models.CharField(max_length=50)
     is_tradable = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -24,7 +24,7 @@ class Deal(models.Model):
     out_quantity = models.FloatField()
     in_quantity = models.IntegerField()
     lot_exchange_rate = models.FloatField()
-    exchange = models.CharField(max_length=255)
+    exchange = models.CharField(max_length=50)
     note = models.TextField(max_length=10000, null=True)
     time_deal = models.DateTimeField(default=datetime.now())
     created = models.DateTimeField(auto_now_add=True)
@@ -45,3 +45,13 @@ class Portfolio(models.Model):
 
     def __str__(self):
         return self.portfolio_name
+
+class Account(models.Model):
+    portfolio = models.ForeignKey('Portfolio', on_delete=models.SET_NULL, null=True)
+    account_name = models.CharField(max_length=50)
+    broker = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.account_name
