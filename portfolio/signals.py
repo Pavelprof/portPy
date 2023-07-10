343e36f701a0352bda688bb30f331da217885153
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-10 19:58:52 +0300

    signals for position updating. fund transaction, in progress

--- portfolio/signals.py
+++ portfolio/signals.py
@@ -1,7 +1,26 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from portfolio.models import Transaction, Position
 
-@receiver(post_save, sender=Transaction)
-def update_positions(sender, instance, created, **kwargs):
-    if created:
+from portfolio.models import Transaction, Deal, Position
+
+@receiver([post_save, post_delete], sender=Transaction)
+def update_positions_from_transaction(sender, instance, **kwargs):
+
+    account = instance.account
+    asset = instance.asset_transaction
+    quantity = instance.quantity_transaction
+
+    try:
+        position = Position.objects.get(account=account, asset=asset)
+
+        if instance.type_transaction == Transaction.Types_transaction.FUND:
+            if kwargs.get('signal') == post_delete:
+                position.quantity_position -= quantity
+            else:
+                position.quantity_position += quantity
+        elif instance.type_transaction == Transaction.Types_transaction.PROFIT:
+            pass
+
+        position.save()
+    except Position.DoesNotExist:
+        Position.objects.create(account=account, asset=asset, quantity_position=quantity)
