f863d3446771f8b12df4f469d176e6a6d9d82b5e
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-07-14 16:14:20 +0300

    position updating from transaction again

--- portfolio/signals.py
+++ portfolio/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from portfolio.models import Transaction, Deal, Position
-from simple_history.models import HistoricalRecords
 
 @receiver([post_save, post_delete], sender=Transaction)
 def update_positions_from_transaction(sender, instance, **kwargs):
@@ -20,9 +19,12 @@ def update_positions_from_transaction(sender, instance, **kwargs):
             position.quantity_position += quantity
             position.save()
         else:
-            previous_instance = Transaction.history.filter(id=instance.id).order_by('-history_date').first()
-            Position.objects.filter(asset=previous_instance.asset_transaction, account=previous_instance.account).update(quantity_position=Position.F('quantity_position') - previous_instance.quantity_transaction)
-            Position.objects.filter(asset=instance.asset_transaction, account=instance.account).update(quantity_position=Position.F('quantity_position') + instance.quantity_transaction)
+            new_trans = instance.history.first()
+            prev_inst = new_trans.prev_record
+            prev_pos = Position.objects.filter(account = prev_inst.account, asset = prev_inst.asset_transaction)
+            prev_pos.update(quantity_position = prev_pos.first().quantity_position - prev_inst.quantity_transaction)
+            new_pos = Position.objects.filter(id = position.id)
+            new_pos.update(quantity_position = new_pos.first().quantity_position + new_trans.quantity_transaction)
 
     except Position.DoesNotExist:
         Position.objects.create(account=account, asset=asset, quantity_position=quantity)
