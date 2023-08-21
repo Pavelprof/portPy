36be4872b31b5a4b41246ff088a331fed1b63f05
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-08-21 19:54:23 +0300

    "get_quotes_from_binance" is done

--- portfolio/quotes_provider.py
+++ portfolio/quotes_provider.py
@@ -29,8 +29,19 @@ def get_quotes_from_moex(ticker_list):
 
     return quotes
 
+
 def get_quotes_from_binance(ticker_list):
-    pass
+    base_url = "https://api.binance.com/api/v3/ticker/price"
+    quotes = {}
+
+    for ticker in ticker_list:
+        response = requests.get(base_url, params={"symbol": ticker})
+        data = response.json()
+        if 'symbol' in data and 'price' in data:
+            quotes[data['symbol']] = float(data['price'])
+
+    return quotes
+
 
 def get_quotes_from_yfinance(ticker_list):
     pass
