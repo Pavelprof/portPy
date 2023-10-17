ca988f2c28f0553bc1b903f45b3a2fc047589b1d
Pavel <34599280+Pavelprof@users.noreply.github.com>
2023-10-17 09:33:11 +0300

    unique_transaction_types method was corrected

--- portfolio/views.py
+++ portfolio/views.py
@@ -29,13 +29,12 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def unique_transaction_types(self, request, *args, **kwargs):
-        # получаем queryset транзакций, принадлежащих текущему пользователю
-        user_transactions = self.queryset.filter(account__portfolio__user=self.request.user)
-
-        # получаем уникальные типы транзакций
+        user_transactions = self.queryset.filter(account__portfolio__user=request.user)
         unique_types = user_transactions.order_by().values_list('type_transaction', flat=True).distinct()
+        type_display_mapping = dict(Transaction.Types_transaction.choices)
+        unique_type_dict = {name: id for id, name in type_display_mapping.items() if id in unique_types}
 
-        return Response(unique_types)
+        return Response(unique_type_dict)
 
 class AssetViewSet(viewsets.ModelViewSet):
     queryset = Asset.objects.all()
