2ba6fca02411ac40c0a7915d18dd73fef985ac56
Pavelprof <vasilevpvl1@gmail.com>
2024-02-05 11:44:03 +0300

    test new pc

--- portpy/settings.py
+++ portpy/settings.py
@@ -195,7 +195,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 30,
+    'PAGE_SIZE': 100,
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
